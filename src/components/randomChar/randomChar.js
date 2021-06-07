cce86aced56c644b7c6e41488d640aac56ceeab0
Dmitrii Bledniuc <ingoodshape93@gmail.com>
2021-06-07 23:53:08 +0300

    spinner

---
+++ src/components/randomChar/randomChar.js
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import GotService from "../../services/gotService";
+import Spinner from "../spinner";
+import "./randomChar.css";
+
+export default class RandomChar extends Component {
+  constructor(props) {
+    super(props);
+    this.updateChar();
+  }
+  gotService = new GotService();
+  state = {
+    char: {},
+    loading: true,
+  };
+  updateChar() {
+    const id = Math.floor(Math.random() * 140 + 10);
+    this.gotService.getCharacter(id).then(this.onCharLoaded);
+  }
+  onCharLoaded = (char) => {
+    this.setState({ char, loading: false });
+  };
+  render() {
+    const {
+      char: { name, gender, born, died, culture },
+      loading,
+    } = this.state;
+    if (loading) {
+      return <Spinner />;
+    }
+    return (
+      <div className="random-block rounded">
+        <h4>Random Character: {name}</h4>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item d-flex justify-content-between">
+            <span className="term">Gender </span>
+            <span>{gender}</span>
+          </li>
+          <li className="list-group-item d-flex justify-content-between">
+            <span className="term">Born </span>
+            <span>{born}</span>
+          </li>
+          <li className="list-group-item d-flex justify-content-between">
+            <span className="term">Died </span>
+            <span>{died}</span>
+          </li>
+          <li className="list-group-item d-flex justify-content-between">
+            <span className="term">Culture </span>
+            <span>{culture}</span>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./spinner.css";
+const Spinner = (props) => {
+  return (
+    <div className="loadingio-spinner-gear-6birz7zefdk">
+      <div className="ldio-q362mx204x">
+        <div>
+          <div />
+          <div />
+          <div />
+          <div />
+          <div />
+          <div />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Spinner;
